08b8088e12a7ea34da2a5c333315a3ad91bf6b72
david <david@f87fd56f-0123-0410-af00-cac8b4cc7d3a>
2006-12-05 15:47:37 +0000

    added status/confirm window for selected text scraper
    git-svn-id: file:///home/ddahl/code/rcache/rcache-orig@27 f87fd56f-0123-0410-af00-cac8b4cc7d3a

--- FFExt/rcache/chrome/content/rcache.js
+++ FFExt/rcache/chrome/content/rcache.js
@@ -46,8 +46,72 @@ var rcache = {
     var txt = rcache.selection();
     //alert(txt);
     if (txt !=""){
-      var result = rcache.post_url('http://zinn.ddahl.com:8000/postcache/',txt);
-      //alert("ran httppost \n" + txt );
+      var result = rcache.post_url('http://zinn.ddahl.com:8000/postcache/',txt);      //fixme: need callback to notify when done... 
+    } else {
+      alert("No Text is Selected");
+    }
+  },
+
+ confirm: function(){
+    var txt = rcache.selection();
+    if (txt !=""){
+      title = rcache.thetitle();
+      var confirmwin = window.open("chrome://rcache/content/rcache_status.xul", 
+				   "status", 
+				   "chrome,width=500,height=170,modal=no" );
+      var loadFunction = function() {
+	selTex =
+	confirmwin.document.getElementById('selectedtext');
+	selTex.setAttribute("value",txt);
+	pgTitle =
+	confirmwin.document.getElementById('pagetitle');
+              pgTitle.setAttribute("value",rcache.thetitle());
+	pgUrl =
+	confirmwin.document.getElementById('url');
+              pgUrl.setAttribute("value",rcache.currentURL());
+      };
+      confirmwin.addEventListener("load", loadFunction, false); 
+    } else {
+      alert("No Text is Selected");
+    }
+  },
+
+ post_after_confirm: function(){
+    //tags not supported on server yet
+    if (document.getElementById('selectedtext').value !=""){
+      var serverurl = 'http://zinn.ddahl.com:8000/postcache/';
+      var http = new XMLHttpRequest();
+
+      var winurl = document.getElementById('url').value;
+      var link = 'entry_url=' + escape(winurl) + '&';
+      
+      var wintitle = document.getElementById('pagetitle').value;
+      var name = 'entry_name=' + escape(wintitle) + '&';
+
+      var windesc = document.getElementById('pagetitle').value;
+      var desc = 'description=' + escape(windesc) + '&';
+
+      var wintext = document.getElementById('selectedtext').value;
+      var text = 'text_content=' + escape(wintext) + '&';
+
+      var wintags = document.getElementById('tags').value;
+      var tags = 'tags=' + escape(wintags)
+
+      var params = link + name + desc + text + tags;
+
+      http.onreadystatechange = function() {
+	//Call a function when the state changes.
+	if(http.readyState == 4 && http.status == 200) {
+	  alert(http.responseText);
+	  window.close();
+	}
+      }
+      http.open("POST", serverurl, true);
+      //Send the proper header infomation along with the request
+      http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+      http.setRequestHeader("Content-length", params.length);
+      http.setRequestHeader("Connection", "close");
+      http.send(params);
     } else {
       alert("No Text is Selected");
     }
@@ -94,7 +158,21 @@ var rcache = {
  view_img_test: function(){
     var itms = browser.contentWindow.document.getElementsByTagName('td');
     alert(itms[0].toString());
+  },
+ 
+ status: function(){
+    window.open("chrome://rcache/content/rcache_status.xul", "bmarks", "chrome,width=300,height=200");
   }
+ //============> TODO <==============\\
+ // 0. Open new tab after async http call to detail screen of 
+ //    new rcache entry
+ // 1. get list of links in selected text or list of links in document
+ //    for upload to entry_urls
+ // 2. get list of images on page, any image over THRESHOLD xy gets pushed up to media table for entry
+ // 3. Collect other data in html source: Metatags, etc...
+ // 
+
+
  //end of object
 };
 
