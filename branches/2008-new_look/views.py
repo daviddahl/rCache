a674d8481adb6cef6f11413bc4516d258b945b34
joshua <joshua@f87fd56f-0123-0410-af00-cac8b4cc7d3a>
2007-11-11 01:12:01 +0000

    killed old imports
    git-svn-id: file:///home/ddahl/code/rcache/rcache-orig@213 f87fd56f-0123-0410-af00-cac8b4cc7d3a

--- branches/2008-new_look/views.py
+++ branches/2008-new_look/views.py
@@ -25,8 +25,11 @@ import pdf
 import html2text
 from email_messages import *
 from twill import get_browser
-from twill.commands import go as tw_go
-from BeautifulSoup import BeautifulSoup
+try:
+    from twill.commands import go as tw_go
+except:
+	tw_go = None
+#from BeautifulSoup import BeautifulSoup
 try:
     if os.environ['RCACHE_USE_TIDY']:
         import tidy
