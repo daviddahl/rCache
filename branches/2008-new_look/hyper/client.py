70fd6535a8fe38a9d22c42785d6da943d737eb6c
david <david@f87fd56f-0123-0410-af00-cac8b4cc7d3a>
2007-12-08 20:07:16 +0000

    removed hyperestraier auth
    git-svn-id: file:///home/ddahl/code/rcache/rcache-orig@239 f87fd56f-0123-0410-af00-cac8b4cc7d3a

--- branches/2008-new_look/hyper/client.py
+++ branches/2008-new_look/hyper/client.py
@@ -10,12 +10,12 @@ class HyperClient(object):
     """
     hyperestraier client to wrap hyperestraier pure python module
     """
-    url = 'http://127.0.0.1:1972/node/rcache'
+    url = 'http://fisk.rcache.com:1972/node/rcache'
     cond = None
     results = []
     node = None
 
-    def __init__(self,url='http://127.0.0.1:1972/node/rcache'):
+    def __init__(self,url='http://fisk.rcache.com:1972/node/rcache'):
         self.url = url
     
     def search(self,query,user_id):
@@ -25,7 +25,7 @@ class HyperClient(object):
         """
         expr = "@author STREQ %s" % user_id
         self.node = h.Node()
-        self.node.set_auth("rcache", "bigrig")
+        #self.node.set_auth("rcache", "bigrig")
         self.node.set_url(self.url)
         self.cond = h.Condition()
         self.cond.set_phrase(query)
