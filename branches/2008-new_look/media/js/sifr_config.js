02d507c5fd5a716f6a31927e433e0cde1e73d250
joshua <joshua@f87fd56f-0123-0410-af00-cac8b4cc7d3a>
2008-03-02 10:21:58 +0000

    massive redesign of entry detail page, SIFR updates, addition of icons for document editing, etc
    git-svn-id: file:///home/ddahl/code/rcache/rcache-orig@259 f87fd56f-0123-0410-af00-cac8b4cc7d3a

--- branches/2008-new_look/media/js/sifr_config.js
+++ branches/2008-new_look/media/js/sifr_config.js
@@ -10,9 +10,97 @@ var bembo = {
 var bemboSEMI = {
   src: '/media/swf/bemboSEMI.swf'
 };
+var myriadCDBD = {
+  src: '/media/swf/myriadCDBD.swf'
+};
+
+var tradecondBD = {
+  src: '/media/swf/tradecondBD.swf'
+};
+
+var unvCOND = {
+  src: '/media/swf/unvCOND.swf'
+};
+
 
 
 sIFR.activate(bembo,bemboSEMI,bulmerMT,bulmerMTSEMI);
+
+
+sIFR.replace(bulmerMT, {
+  	selector: '#text_content_boundry h1'
+    ,wmode:'transparent'
+    ,forceClear:'true'
+    ,forceWidth:'true'
+    ,fitExactly:'true'
+    ,repaintOnResize:'false'
+    ,offsetTop:'-4'
+    ,offsetLeft:'0'
+    ,tuneHeight:'-5'
+    ,tuneWidth:'3'
+    ,css: {
+      '.sIFR-root': { 'leading':'-4','font-size':'43','color':'#272115','letter-spacing':-2,'background-color':'#ffffff','margin-left':0,'margin-right':0,'text-transform':'none'},
+        'a' : {'color': '#272115','text-decoration': 'none'},
+        'a:hover' : {'color': '#272115'}
+    }
+});
+
+// sIFR.replace(myriadCDBD, {
+//   	selector: '#detail_wrapper h3'
+//     ,wmode:'transparent'
+//     ,forceClear:'true'
+//     ,forceWidth:'true'
+//     ,fitExactly:'true'
+//     ,repaintOnResize:'false'
+//     ,offsetTop:'-2'
+//     ,offsetLeft:'0'
+//     ,tuneHeight:'-3'
+//     ,tuneWidth:'3'
+//     ,css: {
+//       '.sIFR-root': { 'leading':'-1','font-size':'15','color':'#E68E3B','letter-spacing':0,'background-color':'#391608','margin-left':0,'margin-right':0,'text-transform':'none'},
+//         'a' : {'color': '#272115','text-decoration': 'none'},
+//         'a:hover' : {'color': '#272115'}
+//     }
+// });
+
+sIFR.replace(myriadCDBD, {
+  	selector: '#nav-wrap h4'
+    ,wmode:'transparent'
+    ,forceClear:'true'
+    ,forceWidth:'true'
+    ,fitExactly:'true'
+    ,repaintOnResize:'false'
+    ,offsetTop:'-2'
+    ,offsetLeft:'0'
+    ,tuneHeight:'-3'
+    ,tuneWidth:'3'
+    ,css: {
+      '.sIFR-root': { 'leading':'-1','font-size':'18','color':'#164354','letter-spacing':0,'background-color':'#D2E2E8','margin-left':0,'margin-right':0,'text-transform':'none'},
+        'a' : {'color': '#272115','text-decoration': 'none'},
+        'a:hover' : {'color': '#272115'}
+    }
+});
+
+
+
+
+sIFR.replace(myriadCDBD, {
+  	selector: '#global_page_title h1'
+    ,wmode:'transparent'
+    ,forceClear:'true'
+    ,forceWidth:'true'
+    ,fitExactly:'true'
+    ,repaintOnResize:'false'
+    ,offsetTop:'-3'
+    ,offsetLeft:'0'
+    ,tuneHeight:'-8'
+    ,tuneWidth:'3'
+    ,css: {
+      '.sIFR-root': { 'leading':'-14','font-size':'30','color':'#E68E3B','letter-spacing':-1,'background-color':'#391608','margin-left':0,'margin-right':0,'text-transform':'uppercase'},
+        'a' : {'color': '#E68E3B','text-decoration': 'none'},
+        'a:hover' : {'color': '#E68E3B'}
+    }
+});
 
 sIFR.replace(bulmerMT, {
   	selector: 'h1'
@@ -26,11 +114,12 @@ sIFR.replace(bulmerMT, {
     ,tuneHeight:'-5'
     ,tuneWidth:'3'
     ,css: {
-      '.sIFR-root': { 'leading':'-14','font-size':'43','color':'#272115','letter-spacing':-2,'background-color':'#E2DEBF','margin-left':0,'margin-right':0,'text-transform':'none'},
+      '.sIFR-root': { 'leading':'-14','font-size':'55','color':'#272115','letter-spacing':-2,'background-color':'#E2DEBF','margin-left':0,'margin-right':0,'text-transform':'none'},
         'a' : {'color': '#272115','text-decoration': 'none'},
         'a:hover' : {'color': '#272115'}
     }
 });
+
 
 
 sIFR.replace(bulmerMT, {
@@ -41,12 +130,12 @@ sIFR.replace(bulmerMT, {
     ,forceWidth:'true'
     ,fitExactly:'true'
     ,repaintOnResize:'false'
-    ,offsetTop:'-8'
+    ,offsetTop:'-6'
     ,offsetLeft:'0'
     ,tuneHeight:'-8'
     ,tuneWidth:'3'
     ,css: {
-      '.sIFR-root': { 'leading':'-14','font-size':'34','color':'#ffffff','letter-spacing':-1,'background-color':'#3F341D','margin-left':0,'margin-right':0,'text-transform':'none','display':'block','text-align':'center'},
+      '.sIFR-root': { 'leading':'-14','font-size':'34','color':'#ffffff','letter-spacing':-1,'background-color':'#1F0F03','margin-left':0,'margin-right':0,'text-transform':'none','display':'block','text-align':'center'},
         'a' : {'color': '#ffffff','text-decoration': 'none'},
         'a:hover' : {'color': '#E2DEBF'}
     }
@@ -64,6 +153,6 @@ sIFR.replace(bulmerMT, {
     ,tuneHeight:'-8'
     ,tuneWidth:'3'
     ,css: {
-      '.sIFR-root': { 'font-size':'11','color':'#E2DEBF','letter-spacing':6,'background-color':'#3F341D','margin-left':0,'margin-right':0,'text-transform':'uppercase','display':'block','text-align':'center'}
+      '.sIFR-root': { 'font-size':'11','color':'#E2DEBF','letter-spacing':6,'background-color':'#1F0F03','margin-left':0,'margin-right':0,'text-transform':'uppercase','display':'block','text-align':'center'}
     }
 });
